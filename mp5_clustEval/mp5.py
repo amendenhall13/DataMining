cd5dd8bb996a3c593600a69f25e419e13cc1804b
labp-miromatrix <80780160+labp-miromatrix@users.noreply.github.com>
2022-03-25 13:22:15 -0500

    Working clustering eval (Jaccard)

--- mp5_clustEval/mp5.py
+++ mp5_clustEval/mp5.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 import collections
 
-class aggCluster:
+class ClusterEval:
     def __init__(self,file):
         self.inArray = []
         self.n = 0
@@ -15,8 +15,8 @@ class aggCluster:
     def runPipe(self,output,tp):
         self.outFilename = output
         self.formatInputArray(tp)
-        self.nmi = self.calcNMI()
-        self.jaccard = self.calcJaccard()
+        self.nmi = format(round(self.calcNMI(),3), '.3f')
+        self.jaccard = format(round(self.calcJaccard(),3),'.3f')
         print(str(self.nmi)+" "+str(self.jaccard))
         
 
@@ -53,7 +53,30 @@ class aggCluster:
         return self.nmi
         
     def calcJaccard(self):
-        pass
+        G = [item[0] for item in self.inArray] 
+        C = [item[1] for item in self.inArray] 
+        cCounter = dict(collections.Counter(C))
+        gCounter = dict(collections.Counter(G))
+        allCounter = dict(collections.Counter(str(elem) for elem in self.inArray))
+        #print(self.inArray)
+        #Calculate true positive
+        tp = 0
+        for count in allCounter:
+            #print(allCounter[count])
+            tp += self.nC2(allCounter[count])
+        #Calculate false negative
+        fn = 0
+        for count in gCounter:
+            fn += self.nC2(gCounter[count])
+        fn -= tp #Subtract true positive
+        #Calculate false positive
+        fp = 0
+        for count in cCounter:
+            fp += self.nC2(cCounter[count])
+        fp -= tp
+        #Calculate Jaccard
+        jac = tp/(tp+fn+fp)
+        return jac
         
     def formatInputArray(self,tp):
         if(tp=="code"): #Read input with pandas from text file for testing.
@@ -80,11 +103,20 @@ class aggCluster:
         '''Puts array values in self.inArray'''
         
 
+    def nC2(self,n):
+        f = math.factorial
+        if n >=2:
+            m = f(n) / (f(2)*f(n-2))
+        #if n == 2:
+        #    m = 1
+        else:
+            m = 0
+        return m
 
 
 def main():
-    file = "mp5_clustEval\\sample1.txt"
-    kmeans = aggCluster(file)
+    file = "mp5_clustEval\\sample2.txt"
+    kmeans = ClusterEval(file)
     kmeans.runPipe("clusters.txt","code")
 
 
