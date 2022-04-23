4c296f507610834422e7b245f1c6adf6701221a3
labp-miromatrix <80780160+labp-miromatrix@users.noreply.github.com>
2022-04-23 13:13:58 -0500

    split candidates

--- mp6_DecisionTree/mp6.py
+++ mp6_DecisionTree/mp6.py
@@ -5,33 +5,86 @@ import collections
 
 class DecisionTree(object):
     def __init__(self,file):
-        
+        self.inputFilename = file
+        self.outFilename = ""
+        self.trainLabels = []
+        self.testLabels = []
+        self.trainAttr = []
+        self.testAttr = []
 
     def runPipe(self,output,tp):
         self.outFilename = output
         self.formatInputArray(tp)
-        self.test = ""
-        self.train = ""
-        
+        [attrKeys, splitCand] = self.findSplitVal()
+        print(attrKeys)
+        print(splitCand)
 
-    def classifyVals(self):
-        pass
+    def findSplitVal(self):
+        all_keys = list(set().union(*(d.keys() for d in self.trainAttr)))
+        splitCandidates = [] #will align with all_keys to show
+        for k in all_keys:
+            #Find all the values of that attribute
+            attrVals = [d[k] for d in self.trainAttr if k in d]
+            splitCand = self.findSplitCandidates(attrVals)
+            splitCandidates.append(splitCand)
+        return [all_keys, splitCandidates]
 
-    def findSplit(self):
-        pass
+    def findSplitCandidates(self,arr):
+        '''Given an array, finds all of the split candidates from it'''
+        setArr = list(set(arr))
+        itemPrev = setArr[0]
+        candList = []
+        for i in range(1,len(setArr)):
+            item = setArr[i]
+            cand = (float(item)+float(itemPrev))/2.0
+            candList.append(cand)
+            itemPrev = item
+        return candList
 
+    
     def calcInfoGain(self):
         pass
-                
+
+    
+    def classifyVals(self):
+        pass  
+
     def formatInputArray(self,tp):
         if(tp=="code"): #Read input with pandas from text file for testing.
             nrows_ = 100
             df = pd.read_csv(self.inputFilename,header=None,sep="\n")#,nrows = nrows_)
             df = df[0].str.split(" ",expand=True)
-            df = df.astype(float)
-            listVals = df.values.tolist()
-            self.inArray = listVals
+            #print(df)
+            trainLabels = []
+            trainAttr = []
+            testLabels = []
+            testAttr = []
+            for i,row in df.iterrows():
+                rowAttr = {}
+                rowLabel = -2
+                for j,item in row.items():
+                    #print(item)
+                    if j == 0:
+                        rowLabel = int(float(item))
+                    else:
+                        #put attr stuff into a dict
+                        valList = item.split(":")
+                        #print(valList)
+                        attrNum = int(float(valList[0].strip(''))) #split attr name and value
+                        attrVal = float(valList[1].strip(''))
+                        rowAttr[attrNum] = attrVal
+                    
+                    #Append row to new data structures based on test or train
+                    #print(type(rowLabel))
+                if(rowLabel == -1):
+                    #+print("hi")
+                    testLabels.append(rowLabel)
+                    testAttr.append(rowAttr)
+                else:
+                    trainLabels.append(rowLabel)
+                    trainAttr.append(rowAttr)            
         if(tp=="hack"):
+            '''Not implemented yet'''
             inArray = []
             stop = False
             while not stop:
@@ -44,15 +97,18 @@ class DecisionTree(object):
                 except:
                     stop = True
             self.inArray = inArray
-        self.n = len(self.inArray)
-        '''Puts array values in self.inArray'''
+        
+        self.testLabels = testLabels
+        self.testAttr = testAttr
+        self.trainLabels = trainLabels
+        self.trainAttr = trainAttr
         
 
 
 def main():
-    file = "mp5_clustEval\\sample2.txt"
+    file = "mp6_DecisionTree\\sample0.txt"
     dTree = DecisionTree(file)
-    dTree.runPipe("clusters.txt","code")
+    dTree.runPipe("output.txt","code")
 
 
 if __name__=="__main__":
