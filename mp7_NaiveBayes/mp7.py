a7cc71f5917e0bfb2cbd42ee3869a2975192456c
labp-miromatrix <80780160+labp-miromatrix@users.noreply.github.com>
2022-04-24 18:15:37 -0500

    Sample0 working

--- mp7_NaiveBayes/mp7.py
+++ mp7_NaiveBayes/mp7.py
@@ -14,12 +14,15 @@ class NaiveBayes(object):
         self.attrList = [] #len(#attr), all attr names, done in formatInputArray
         #[2,2,2,2] -- 2 for booleans, 6 for leg
         self.optionsByVar = [2,2,2,2,2,2,2,2,2,2,2,2,6,2,2,2] #How many options does each var have? len(#attr)
+        self.optionsList = [] #Made in makeConfusionMatrix
 
         #[1,2,3,4,6,7] -- all possible class labels, sorted
         self.classList = [1,2,3,4,5,6,7] #done in formatInputArray
         #[3,3,3,3,3] -- counts for each class matched to classList order.
         self.countByClass = [] #len(#classes), done in calcpOfc
-        
+        #[class1->[ctAttr1,ctAttr2],
+        # class2->[ctAttr1,ctAttr2]]...
+        self.confusionMatrix = []
         
         self.pOfc = [] #len(#classes)
         self.pOfXUnderC = [] #len(#classes)
@@ -27,15 +30,63 @@ class NaiveBayes(object):
     def runPipe(self,output,tp):
         [trainVals,trainLabels,testVals] = self.formatInputArray(tp)
         self.pOfc = self.calcpOfc(trainLabels)
-        '''for testVal in unknownList:
-            self.pOfXUnderC = self.calcpOfxUnderc(testVal)
+        self.confusionMatrix = self.makeConfusionMatrix(trainVals,trainLabels)
+        for test in testVals:
+            self.pOfXUnderC = self.calcpOfxUnderc(test)
             totalProbList = [a*b for a,b in zip(self.pOfc,self.pOfXUnderC)] #element by element matrix multiplication
             maxProb = max(totalProbList)
             #Linked classList/probList to find index and therefore find class.
             cls = self.classList[totalProbList.index(maxProb)] 
-            print(cls)'''
+            print(cls)
 
+    def calcpOfxUnderc(self,testVal):
+        '''Return array of len(#classes) with L-corrected probabilty of x under c'''
+        probMatrix = []
+        for i,cls in enumerate(self.classList):
+            probForOneClass = []
+            for j,attr in enumerate(self.attrList):
+                confRow = i #ClassList is the row index for the confusion matrix as well as the probabilty matrix
+                #testVal linked to attrList to find confCol
+                confCol = self.optionsList.index(str(attr)+str(int(testVal[j])))
+                countpXC = self.confusionMatrix[confRow][confCol]
+                countC = self.countByClass[i] #Linked to classList
+                numFeatures = self.optionsByVar[j] #Linked to attrList
+                prob = (countpXC+0.1)/(countC+0.1*numFeatures)
+                probForOneClass.append(prob)
+            probMatrix.append(probForOneClass)
+        pXunderC = []
+        for row in probMatrix:
+            colProduct = 1
+            for col in row:
+                colProduct *= col
+            pXunderC.append(colProduct)
+        return pXunderC
+            
+            
+                
 
+    def makeConfusionMatrix(self,trainVals,trainLabels):
+        '''Create the confusion matrix of the counts first so don't have to repeatedly loop through'''
+        #Make the optionsList by filling in 0/1 for all values and then changing the non-boolean one
+        for i,attr in enumerate(self.attrList):
+            for i in range(self.optionsByVar[i]): #loop through the number of times the var is, i.e. 1-6
+                self.optionsList.append(attr+str(i))
+        #replace legs numbers with appropriate one
+        self.optionsList = list(map(lambda x: x.replace("legs1", 'legs6'), self.optionsList))
+        self.optionsList = list(map(lambda x: x.replace("legs3", 'legs8'), self.optionsList))
+        
+        #Make confusion matrix of 0s
+        confusionMatrix = [ [0]*len(self.optionsList) for _ in range(len(self.classList)) ]
+        #Go through training values/labels and add onto the appropriate matrix locations
+        for i,dataLabel in enumerate(trainLabels):
+            confRow = self.classList.index(dataLabel) #The confusion matrix row index is looked up from the classList
+            #Create col lookup in confMatrix by adding the attrList lookup to the value at that index in the attrList
+            for j,attrVal in enumerate(trainVals[i]):
+                attrLabel = self.attrList[j]
+                confColString = attrLabel + str(int(attrVal))
+                confCol = self.optionsList.index(confColString)
+                confusionMatrix[confRow][confCol] += 1
+        return confusionMatrix
 
     def calcpOfc(self,trainLabels):
         '''Return an array of len(#classes) with Laplacian-corrected probability of each.
@@ -53,17 +104,6 @@ class NaiveBayes(object):
             prob = (self.countByClass[i]+0.1)/(totalSum+0.1*numClassesPresent)
             pOfc.append(prob)
         return pOfc
-
-
-
-            
-
-        
-
-    def calcpOfxUnderc(self,testVal):
-        '''Return array of len(#classes) with L-corrected probabilty of x under c'''
-        pass
-
 
     def formatInputArray(self,tp):
         if(tp=="code"): #Read input with pandas from text file for testing.
